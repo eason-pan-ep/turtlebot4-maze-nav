eab91b39d69a295cc3c24c22b0b7296e6a56f800
eason-pan-ep <eason.pan@proton.me>
2025-04-13 20:57:51 -0700

    add safety params adjustment node and test

--- turtlebot4_ws/src/turtlebot4_safety/setup.py
+++ turtlebot4_ws/src/turtlebot4_safety/setup.py
@@ -1,26 +1,29 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
-package_name = 'final_project_5335_jel'  # Changed to match directory name
+package_name = 'turtlebot4_safety'  # Changed to match directory name
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version='0.1.0',
+     packages=[package_name, f'{package_name}.scripts'],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='eason',
     maintainer_email='eason.pan@proton.me',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='maze navigation project',
+    license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'final_project_first_node = final_project_5335_jel.final_project_first_node:main'  # Use underscores
+            'safety_params = turtlebot4_safety.scripts.safety_params:main'
         ],
     },
 )
